1e319f88ff199c81657afccf8330d26e22d953f6
Daniel Cheng <zetafunction@gmail.com>
2022-12-18 02:05:43 -0800

    Day 18: make implementation (much) nicer.

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -13,18 +13,129 @@
 //  limitations under the License.
 
 use aoc_2022::{oops, oops::Oops};
+use std::borrow::Borrow;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::io::{self, Read};
+use std::ops::Add;
 use std::str::FromStr;
 
-#[derive(Clone, Copy, Eq, PartialEq)]
-enum Point {
-    Side,
-    Cube,
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+struct Point3 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+const NEIGHBOR_VECTORS: &[Vector3] = &[
+    Vector3::new(-1, 0, 0),
+    Vector3::new(1, 0, 0),
+    Vector3::new(0, -1, 0),
+    Vector3::new(0, 1, 0),
+    Vector3::new(0, 0, -1),
+    Vector3::new(0, 0, 1),
+];
+
+struct Neighbors<'a> {
+    p: &'a Point3,
+    iter: std::slice::Iter<'static, Vector3>,
+}
+
+impl<'a> Iterator for Neighbors<'a> {
+    type Item = Point3;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(v) = self.iter.next() {
+            Some(*self.p + *v)
+        } else {
+            None
+        }
+    }
+}
+
+impl Point3 {
+    const fn new(x: i32, y: i32, z: i32) -> Self {
+        Point3 { x, y, z }
+    }
+
+    fn neighbors(&self) -> Neighbors {
+        Neighbors {
+            p: self,
+            iter: NEIGHBOR_VECTORS.iter(),
+        }
+    }
+}
+
+impl Add<Vector3> for Point3 {
+    type Output = Point3;
+
+    fn add(self, rhs: Vector3) -> Self::Output {
+        Point3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+struct Vector3 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Vector3 {
+    const fn new(x: i32, y: i32, z: i32) -> Self {
+        Vector3 { x, y, z }
+    }
+}
+
+// TODO: Maybe this should be a cube class?
+struct Bounds3 {
+    min_x: i32,
+    max_x: i32,
+    min_y: i32,
+    max_y: i32,
+    min_z: i32,
+    max_z: i32,
+}
+
+impl Bounds3 {
+    fn contains(&self, p: &Point3) -> bool {
+        p.x >= self.min_x
+            && p.x <= self.max_x
+            && p.y >= self.min_y
+            && p.y <= self.max_y
+            && p.z >= self.min_z
+            && p.z <= self.max_z
+    }
+
+    fn from_point3s<I>(i: I) -> Self
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Point3>,
+    {
+        i.into_iter()
+            .fold(Self::new_uninitialized(), |b, p| Bounds3 {
+                min_x: std::cmp::min(b.min_x, p.borrow().x - 1),
+                max_x: std::cmp::max(b.max_x, p.borrow().x + 1),
+                min_y: std::cmp::min(b.min_y, p.borrow().y - 1),
+                max_y: std::cmp::max(b.max_y, p.borrow().y + 1),
+                min_z: std::cmp::min(b.min_z, p.borrow().z - 1),
+                max_z: std::cmp::max(b.max_z, p.borrow().z + 1),
+            })
+    }
+
+    fn new_uninitialized() -> Self {
+        Bounds3 {
+            min_x: i32::MAX,
+            max_x: i32::MIN,
+            min_y: i32::MAX,
+            max_y: i32::MIN,
+            min_z: i32::MAX,
+            max_z: i32::MIN,
+        }
+    }
 }
 
 struct Puzzle {
-    points: HashSet<(i64, i64, i64)>,
+    points: Vec<Point3>,
 }
 
 impl FromStr for Puzzle {
@@ -36,10 +147,10 @@ impl FromStr for Puzzle {
                 .lines()
                 .map(|line| {
                     let mut parser = line.split(',');
-                    let x = parser.next().unwrap().parse()?;
-                    let y = parser.next().unwrap().parse()?;
-                    let z = parser.next().unwrap().parse()?;
-                    Ok((x, y, z))
+                    let x = parser.next().ok_or_else(|| oops!("missing x"))?.parse()?;
+                    let y = parser.next().ok_or_else(|| oops!("missing y"))?.parse()?;
+                    let z = parser.next().ok_or_else(|| oops!("missing z"))?.parse()?;
+                    Ok(Point3::new(x, y, z))
                 })
                 .collect::<Result<_, Oops>>()?,
         })
@@ -50,141 +161,51 @@ fn parse(input: &str) -> Result<Puzzle, Oops> {
     input.parse()
 }
 
-fn part1(puzzle: &Puzzle) -> Result<i32, Oops> {
-    let mut sides = HashMap::<(i64, i64, i64), i32>::new();
-    for (x, y, z) in &puzzle.points {
-        let mut count = 6;
-        if let Some(left) = sides.get_mut(&(*x - 1, *y, *z)) {
-            *left -= 1;
-            count -= 1;
-        }
-        if let Some(right) = sides.get_mut(&(*x + 1, *y, *z)) {
-            *right -= 1;
-            count -= 1;
-        }
-        if let Some(bottom) = sides.get_mut(&(*x, *y - 1, *z)) {
-            *bottom -= 1;
-            count -= 1;
-        }
-        if let Some(top) = sides.get_mut(&(*x, *y + 1, *z)) {
-            *top -= 1;
-            count -= 1;
-        }
-        if let Some(front) = sides.get_mut(&(*x, *y, *z - 1)) {
-            *front -= 1;
-            count -= 1;
-        }
-        if let Some(back) = sides.get_mut(&(*x, *y, *z + 1)) {
-            *back -= 1;
-            count -= 1;
-        }
-        sides.insert((*x, *y, *z), count);
+fn part1(puzzle: &Puzzle) -> i32 {
+    let mut counts = HashMap::<Point3, i32>::new();
+    for p in &puzzle.points {
+        let count: i32 = p
+            .neighbors()
+            .map(|neighbor| {
+                if let Some(neighbor_count) = counts.get_mut(&neighbor) {
+                    *neighbor_count -= 1;
+                    0
+                } else {
+                    1
+                }
+            })
+            .sum();
+        counts.insert(*p, count);
     }
-    Ok(sides.iter().map(|(_, v)| v).sum::<i32>())
+    counts.iter().map(|(_, v)| v).sum()
 }
 
-fn part2(puzzle: &Puzzle) -> Result<i32, Oops> {
-    let mut sides = HashMap::<(i64, i64, i64), i32>::new();
-    let mut min_x = i64::MAX;
-    let mut max_x = i64::MIN;
-    let mut min_y = i64::MAX;
-    let mut max_y = i64::MIN;
-    let mut min_z = i64::MAX;
-    let mut max_z = i64::MIN;
-    for (x, y, z) in &puzzle.points {
-        min_x = std::cmp::min(min_x, *x);
-        max_x = std::cmp::max(max_x, *x);
-        min_y = std::cmp::min(min_y, *y);
-        max_y = std::cmp::max(max_y, *y);
-        min_z = std::cmp::min(min_z, *z);
-        max_z = std::cmp::max(max_z, *z);
-        let mut count = 6;
-        if let Some(left) = sides.get_mut(&(*x - 1, *y, *z)) {
-            *left -= 1;
-            count -= 1;
-        }
-        if let Some(right) = sides.get_mut(&(*x + 1, *y, *z)) {
-            *right -= 1;
-            count -= 1;
-        }
-        if let Some(bottom) = sides.get_mut(&(*x, *y - 1, *z)) {
-            *bottom -= 1;
-            count -= 1;
-        }
-        if let Some(top) = sides.get_mut(&(*x, *y + 1, *z)) {
-            *top -= 1;
-            count -= 1;
-        }
-        if let Some(front) = sides.get_mut(&(*x, *y, *z - 1)) {
-            *front -= 1;
-            count -= 1;
-        }
-        if let Some(back) = sides.get_mut(&(*x, *y, *z + 1)) {
-            *back -= 1;
-            count -= 1;
-        }
-        sides.insert((*x, *y, *z), count);
-    }
-    println!(
-        "{} {} {} {} {} {}",
-        min_x, max_x, min_y, max_y, min_z, max_z
-    );
+fn part2(puzzle: &Puzzle) -> i32 {
+    // Find the bounding box for the puzzle points.
+    let bounds = Bounds3::from_point3s(puzzle.points.iter());
+    let points: HashSet<_> = puzzle.points.iter().collect();
+
+    // Do a BFS from a point guaranteed to be outside the solid and find all reachable surfaces.
     let mut count = 0;
-    min_x -= 1;
-    max_x += 1;
-    min_y -= 1;
-    max_y += 1;
-    min_z -= 1;
-    max_z += 1;
     let mut frontier = VecDeque::new();
     let mut visited = HashSet::new();
-    frontier.push_back((-min_x, -min_y, -min_z));
-    let in_bounds =
-        |x, y, z| x >= min_x && x <= max_x && y >= min_y && y <= max_y && z >= min_z && z <= max_z;
-    while let Some((cur_x, cur_y, cur_z)) = frontier.pop_front() {
-        if visited.contains(&(cur_x, cur_y, cur_z)) {
-            continue;
-        }
-        visited.insert((cur_x, cur_y, cur_z));
-        let potential_next = vec![
-            (cur_x - 1, cur_y, cur_z),
-            (cur_x + 1, cur_y, cur_z),
-            (cur_x, cur_y - 1, cur_z),
-            (cur_x, cur_y + 1, cur_z),
-            (cur_x, cur_y, cur_z - 1),
-            (cur_x, cur_y, cur_z + 1),
-        ];
-        let potential_next = potential_next
-            .into_iter()
-            .filter(|&(x, y, z)| {
-                in_bounds(x, y, z)
-                    && !visited.contains(&(x, y, z))
-                    && !sides.contains_key(&(x, y, z))
-            })
-            .collect::<Vec<_>>();
+    frontier.push_back(Point3::new(bounds.min_x, bounds.min_y, bounds.min_z));
+    visited.insert(*frontier.front().unwrap());
 
-        if sides.contains_key(&(cur_x - 1, cur_y, cur_z)) {
-            count += 1;
+    while let Some(p) = frontier.pop_front() {
+        for neighbor in p.neighbors() {
+            if !bounds.contains(&neighbor) || visited.contains(&neighbor) {
+                continue;
+            }
+            if points.contains(&neighbor) {
+                count += 1;
+                continue;
+            }
+            frontier.push_back(neighbor);
+            visited.insert(neighbor);
         }
-        if sides.contains_key(&(cur_x + 1, cur_y, cur_z)) {
-            count += 1;
-        }
-
-        if sides.contains_key(&(cur_x, cur_y - 1, cur_z)) {
-            count += 1;
-        }
-        if sides.contains_key(&(cur_x, cur_y + 1, cur_z)) {
-            count += 1;
-        }
-        if sides.contains_key(&(cur_x, cur_y, cur_z - 1)) {
-            count += 1;
-        }
-        if sides.contains_key(&(cur_x, cur_y, cur_z + 1)) {
-            count += 1;
-        }
-        frontier.extend(potential_next.into_iter());
     }
-    Ok(count)
+    count
 }
 
 fn main() -> Result<(), Oops> {
@@ -194,8 +215,8 @@ fn main() -> Result<(), Oops> {
 
     let puzzle = parse(&input)?;
 
-    println!("{}", part1(&puzzle)?);
-    println!("{}", part2(&puzzle)?);
+    println!("{}", part1(&puzzle));
+    println!("{}", part2(&puzzle));
 
     Ok(())
 }
@@ -211,11 +232,11 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(64, part1(&parse(SAMPLE).unwrap()).unwrap());
+        assert_eq!(64, part1(&parse(SAMPLE).unwrap()));
     }
 
     #[test]
     fn example2() {
-        assert_eq!(58, part2(&parse(SAMPLE).unwrap()).unwrap());
+        assert_eq!(58, part2(&parse(SAMPLE).unwrap()));
     }
 }
