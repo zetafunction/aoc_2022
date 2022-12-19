c30dbedfad746df011e4725e6fa8957b42bbc49c
Daniel Cheng <zetafunction@gmail.com>
2022-12-18 21:34:23 -0800

    Day 19: initial too slow recursive solution.

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -25,6 +25,175 @@ struct Blueprint {
     geode_robot_obsidian_cost: i32,
 }
 
+#[derive(Clone, Copy)]
+struct Resources {
+    ore: i32,
+    clay: i32,
+    obsidian: i32,
+    geode: i32,
+}
+
+impl Resources {
+    fn new() -> Self {
+        Resources {
+            ore: 0,
+            clay: 0,
+            obsidian: 0,
+            geode: 0,
+        }
+    }
+
+    fn consume(&self, ore: i32, clay: i32, obsidian: i32) -> Self {
+        Resources {
+            ore: self.ore - ore,
+            clay: self.clay - clay,
+            obsidian: self.obsidian - obsidian,
+            geode: self.geode,
+        }
+    }
+
+    fn maybe_build_ore_robot(&self, blueprint: &Blueprint) -> Option<Resources> {
+        if self.ore < blueprint.ore_robot_ore_cost {
+            None
+        } else {
+            Some(self.consume(blueprint.ore_robot_ore_cost, 0, 0))
+        }
+    }
+
+    fn maybe_build_clay_robot(&self, blueprint: &Blueprint) -> Option<Resources> {
+        if self.ore < blueprint.clay_robot_ore_cost {
+            None
+        } else {
+            Some(self.consume(blueprint.clay_robot_ore_cost, 0, 0))
+        }
+    }
+
+    fn maybe_build_obsidian_robot(&self, blueprint: &Blueprint) -> Option<Resources> {
+        if self.ore < blueprint.obsidian_robot_ore_cost
+            || self.clay < blueprint.obsidian_robot_clay_cost
+        {
+            None
+        } else {
+            Some(self.consume(
+                blueprint.obsidian_robot_ore_cost,
+                blueprint.obsidian_robot_clay_cost,
+                0,
+            ))
+        }
+    }
+
+    fn maybe_build_geode_robot(&self, blueprint: &Blueprint) -> Option<Resources> {
+        if self.ore < blueprint.geode_robot_ore_cost
+            || self.obsidian < blueprint.geode_robot_obsidian_cost
+        {
+            None
+        } else {
+            Some(self.consume(
+                blueprint.geode_robot_ore_cost,
+                0,
+                blueprint.geode_robot_obsidian_cost,
+            ))
+        }
+    }
+
+    fn collect(
+        &self,
+        ore_robots: i32,
+        clay_robots: i32,
+        obsidian_robots: i32,
+        geode_robots: i32,
+    ) -> Resources {
+        Resources {
+            ore: self.ore + ore_robots,
+            clay: self.clay + clay_robots,
+            obsidian: self.obsidian + obsidian_robots,
+            geode: self.geode + geode_robots,
+        }
+    }
+}
+
+impl Blueprint {
+    fn solve_for_max_geodes(
+        &self,
+        resources: Resources,
+        ore_robots: i32,
+        clay_robots: i32,
+        obsidian_robots: i32,
+        geode_robots: i32,
+        time_left: i32,
+    ) -> i32 {
+        if time_left == 0 {
+            return resources.geode;
+        }
+
+        let mut best = resources.geode;
+        if let Some(resources) = resources.maybe_build_ore_robot(self) {
+            let new_resources =
+                resources.collect(ore_robots, clay_robots, obsidian_robots, geode_robots);
+            let result = self.solve_for_max_geodes(
+                new_resources,
+                ore_robots + 1,
+                clay_robots,
+                obsidian_robots,
+                geode_robots,
+                time_left - 1,
+            );
+            best = std::cmp::max(best, result);
+        }
+        if let Some(resources) = resources.maybe_build_clay_robot(self) {
+            let new_resources =
+                resources.collect(ore_robots, clay_robots, obsidian_robots, geode_robots);
+            let result = self.solve_for_max_geodes(
+                new_resources,
+                ore_robots,
+                clay_robots + 1,
+                obsidian_robots,
+                geode_robots,
+                time_left - 1,
+            );
+            best = std::cmp::max(best, result);
+        }
+        if let Some(resources) = resources.maybe_build_obsidian_robot(self) {
+            let new_resources =
+                resources.collect(ore_robots, clay_robots, obsidian_robots, geode_robots);
+            let result = self.solve_for_max_geodes(
+                new_resources,
+                ore_robots,
+                clay_robots,
+                obsidian_robots + 1,
+                geode_robots,
+                time_left - 1,
+            );
+            best = std::cmp::max(best, result);
+        }
+        if let Some(resources) = resources.maybe_build_geode_robot(self) {
+            let new_resources =
+                resources.collect(ore_robots, clay_robots, obsidian_robots, geode_robots);
+            let result = self.solve_for_max_geodes(
+                new_resources,
+                ore_robots,
+                clay_robots,
+                obsidian_robots,
+                geode_robots + 1,
+                time_left - 1,
+            );
+            best = std::cmp::max(best, result);
+        }
+        best = std::cmp::max(
+            best,
+            self.solve_for_max_geodes(
+                resources.collect(ore_robots, clay_robots, obsidian_robots, geode_robots),
+                ore_robots,
+                clay_robots,
+                obsidian_robots,
+                geode_robots,
+                time_left - 1,
+            ),
+        );
+        return best;
+    }
+}
+
 impl FromStr for Blueprint {
     type Err = Oops;
 
@@ -62,8 +231,15 @@ fn parse(input: &str) -> Result<Puzzle, Oops> {
     input.parse()
 }
 
-fn part1(puzzle: &Puzzle) -> Result<usize, Oops> {
-    Ok(0)
+fn part1(puzzle: &Puzzle) -> Result<i32, Oops> {
+    Ok(puzzle
+        .blueprints
+        .iter()
+        .enumerate()
+        .map(|(i, blueprint)| {
+            i as i32 * blueprint.solve_for_max_geodes(Resources::new(), 1, 0, 0, 0, 24)
+        })
+        .sum())
 }
 
 fn part2(puzzle: &Puzzle) -> Result<usize, Oops> {
@@ -102,7 +278,7 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(3579124689, part1(&parse(SAMPLE).unwrap()).unwrap());
+        assert_eq!(33, part1(&parse(SAMPLE).unwrap()).unwrap());
     }
 
     #[test]
